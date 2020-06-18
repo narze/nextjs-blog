d85caf0e0f479f6720537809cd01b048afcd5182
Manassarn Manoonchai <manassarn@gmail.com>
2020-06-18 22:27:08 +0700

    :sparkles: Add #100DaysOfCode ribbon

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ export default function Home() {
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/hundred-days-of-code-css-ribbon@v0.1.1/100-days-of-code-ribbon.css"/>
       </Head>
       <section className={utilStyles.headingMd}>
         <p>Hello!</p>
@@ -15,6 +16,8 @@ export default function Home() {
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
+
+      <a class="hundred-days-of-code-ribbon" href="https://github.com/narze/100-days-of-code" data-day="16" target="_blank"></a>
     </Layout>
   )
 }
