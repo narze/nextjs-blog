eb873d311dc0d9315889e30e3453af0ddd39e69e
Manassarn Manoonchai <manassarn@gmail.com>
2020-06-29 20:01:04 +0700

    :sparkles: Convert project to typescript

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,29 @@
 import Head from 'next/head'
-import Link from 'next/link'
-import Date from '../components/date.js'
-
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
+import Link from 'next/link'
+import Date from '../components/date'
+import { GetStaticProps } from 'next'
 
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
-  return {
-    props: {
-      allPostsData
-    }
-  }
-}
-
-export default function Home({ allPostsData }) {
+export default function Home({
+  allPostsData
+}: {
+  allPostsData: {
+    date: string
+    title: string
+    id: string
+  }[]
+}) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/hundred-days-of-code-css-ribbon@v0.1.1/100-days-of-code-ribbon.css"/>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello!</p>
+        <p>[Your Self Introduction]</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
+          (This is a sample website - you’ll be building a site like this in{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
@@ -45,8 +43,15 @@ export default function Home({ allPostsData }) {
           ))}
         </ul>
       </section>
-
-      <a className="hundred-days-of-code-ribbon" href="https://github.com/narze/100-days-of-code" data-day="17" target="_blank"></a>
     </Layout>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
 }
